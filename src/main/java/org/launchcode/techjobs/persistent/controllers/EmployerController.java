1d969a49e263fb9c0238299e8b3c1e0607811ccd
Trisha <trishaposey@gmail.com>
2023-06-01 20:01:28 -0500

    working on test two

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,6 +15,16 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+//    stopped here at bottom of page that has "controllers" on it
+    @GetMapping
+    public String displayAllEmployers (Model model) {
+        model.addAttribute("title", "All Employers");
+        model.addAttribute("categories", employerRepository.findAll());
+        return "employers/index";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min=2, max=100, message = "Location must be between 2 and 100 characters")
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
